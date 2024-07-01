89cba0a9d2c539a6ef92634c5fce9de20386c14d
Sajen Tuladhar <sajentuladhar9@gmail.com>
2024-07-01 18:25:33 +0545

    coffee strength and sugar incrementation with images

--- lib/coffee_prefs.dart
+++ lib/coffee_prefs.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
 
-class CoffeePrefs extends StatelessWidget {
+class CoffeePrefs extends StatefulWidget {
   const CoffeePrefs({super.key});
+
+  @override
+  State<CoffeePrefs> createState() => _CoffeePrefsState();
+}
+
+class _CoffeePrefsState extends State<CoffeePrefs> {
+  int strength = 1;
+
+  int sugar =0;
+
+  void increaseStrength(){
+    setState((){
+      strength = strength <5 ? strength +1 : 1;
+    });
+  }
+
+  void increaseSugar(){
+     setState((){
+      sugar = sugar <5 ? sugar +1 : 0;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,26 +31,45 @@ class CoffeePrefs extends StatelessWidget {
         Row(
           children: [
             const Text("Strength:"),
-            const Text("3") , // will be dynamically getting this later
-            Image.asset('assets/img/coffee_bean.png',
-            width: 25,
-            color: Colors.brown[100],
-            colorBlendMode: BlendMode.multiply, // this blendmode removes background for the image as the color of the image is set to the color of the container
-            ),
-             const Expanded(child: SizedBox()),
-            const Text('+') // Is gonna be a button later to increment
+            
+            for(int i = 0; i<strength; i++)
+              Image.asset('assets/img/coffee_bean.png',
+              width: 25,
+              color: Colors.brown[100],
+              colorBlendMode: BlendMode.multiply, // this blendmode removes background for the image as the color of the image is set to the color of the container
+              ),
+
+            const Expanded(child: SizedBox()),
+            FilledButton(
+              style: FilledButton.styleFrom(
+                backgroundColor: Colors.brown,
+                foregroundColor: Colors.white,
+              ),
+             onPressed: increaseStrength,
+             child:const  Text('+'))
           ],
         ),
         Row(
           children: [
-            const  Text("Sugars:"),
-            const Text("3") , // will be dynamically getting this later
-             Image.asset('assets/img/sugar_cube.png', 
-             width: 25,
-             color: Colors.brown[100],
-            colorBlendMode: BlendMode.multiply,),
+            const Text('Sugars :'),
+
+            if (sugar == 0)
+              const Text('No sugars'),
+
+            for(int i = 0; i<sugar; i++)
+              Image.asset('assets/img/sugar_cube.png', 
+              width: 25,
+              color: Colors.brown[100],
+              colorBlendMode: BlendMode.multiply,
+            ),
             const Expanded(child:  SizedBox()),
-            const Text('+')// is gonna be button later to increment 
+            FilledButton(
+              style: FilledButton.styleFrom(
+                backgroundColor: Colors.brown,
+                foregroundColor: Colors.white,
+              ),
+              onPressed: increaseSugar,
+             child:const  Text('+'))
           ],
         )
       ],
