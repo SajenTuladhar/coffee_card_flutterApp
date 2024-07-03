0dfc88d07d413532087aebd870d20c727bf9366e
Sajen Tuladhar <sajentuladhar9@gmail.com>
2024-07-03 16:31:19 +0545

    add re-useable text widget

--- lib/coffee_prefs.dart
+++ lib/coffee_prefs.dart
@@ -1,3 +1,4 @@
+import 'package:coffee_card/styled_body_text.dart';
 import 'package:flutter/material.dart';
 
 class CoffeePrefs extends StatefulWidget {
@@ -30,7 +31,7 @@ class _CoffeePrefsState extends State<CoffeePrefs> {
       children: [
         Row(
           children: [
-            const Text("Strength:"),
+            const StyledBodyText("Strength:"),
             
             for(int i = 0; i<strength; i++)
               Image.asset('assets/img/coffee_bean.png',
@@ -51,10 +52,10 @@ class _CoffeePrefsState extends State<CoffeePrefs> {
         ),
         Row(
           children: [
-            const Text('Sugars :'),
+            const StyledBodyText('Sugars :'),
 
             if (sugar == 0)
-              const Text('No sugars'),
+              const StyledBodyText('No sugars'),
 
             for(int i = 0; i<sugar; i++)
               Image.asset('assets/img/sugar_cube.png', 
